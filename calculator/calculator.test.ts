23f74ec315998e33db14c559039d10ace01143b2
Nicolas Rodriguez <nicolasrodriguezch@hotmail.com>
2023-08-29 11:28:37 -0500

    adds more functionality to the calculator

--- calculator/calculator.test.ts
+++ calculator/calculator.test.ts
@@ -1,5 +1,17 @@
-import { add } from "./calculator";
+import { add, divide, multiply, subtract } from "./calculator";
 
-test("add function", ()=> {
+test("add function", () => {
   expect(add(1, 2)).toBe(3);
+});
+test("substract function", () => {
+  expect(subtract(5, 2)).toBe(3);
+});
+test("multiply function", () => {
+  expect(multiply(3, 5)).toBe(15);
+});
+test("divide function", () => {
+  expect(divide(15, 5)).toBe(3);
+});
+test("divide by zero", () => {
+  expect(() => divide(8, 0)).toThrow("Cannot divide by zero!");
 });
